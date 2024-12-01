5206c37f166c5ce297f5a0a92f2c6c794145fa23
karlrobeck <119088381+karlrobeck@users.noreply.github.com>
2024-12-01 18:33:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var member = {
+    name: 'John Doe',
+    age: 32,
+    skills: ['HTML', 'CSS', 'JS'],
+    details: {
+      address: '123 Main St',
+      city: 'Boston'
+    }
+  };
+  return member;
+}
